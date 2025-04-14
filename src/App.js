423a03798e253c6cf628477493648070ef96424c
Harshitha07 <andojiharshitha194@gmail.com>
2025-04-14 15:55:24 +0530

    App.js change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <h1>HELLO! HARSHITHA</h1>
+      <h1>HELLO! HARSHITHA</h1>
     </div>
   );
 }
